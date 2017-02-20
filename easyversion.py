e05bbe6718d0a7ff018beb3c63a88dc59f633804
Susu <susu.0823@gmail.com>
2017-02-20 21:24:49 +0100

    done

--- easyversion.py
+++ easyversion.py
@@ -13,34 +13,34 @@ with open('data.csv') as csvfile:
 
     @route('/origin/name')
     def name():
-        sortname = sorted(mydata[1:], key=operator.itemgetter(0))
-        mydata[1:] = sortname
+        sort = sorted(mydata[1:], key=operator.itemgetter(0))
+        mydata[1:] = sort
         return template('distable', rows=mydata)
 
 
     @route('/origin/age1')
     def age1():
-        sortage1 = sorted(mydata[1:], key=operator.itemgetter(1))
-        mydata[1:] = sortage1
+        sort= sorted(mydata[1:], key=operator.itemgetter(1))
+        mydata[1:] = sort
         return template('distable', rows=mydata)
 
 
     @route('/origin/age2')
     def age2():
-       sortage2 = sorted(mydata[1:], key=operator.itemgetter(1), reverse=True)
-       mydata[1:] = sortage2
+       sort= sorted(mydata[1:], key=operator.itemgetter(1), reverse=True)
+       mydata[1:] = sort
        return template('distable', rows=mydata)
 
     @route('/origin/salary1')
     def salary1():
-       sortsalary1 = sorted(mydata[1:], key=operator.itemgetter(2))
-       mydata[1:] = sortsalary1
+       sort = sorted(mydata[1:], key=operator.itemgetter(2))
+       mydata[1:] = sort
        return template('distable', rows=mydata)
 
     @route('/origin/salary2')
     def salary2():
-      sortsalary2= sorted(mydata[1:], key=operator.itemgetter(2), reverse=True)
-      mydata[1:] = sortsalary2
+      sort= sorted(mydata[1:], key=operator.itemgetter(2), reverse=True)
+      mydata[1:] = sort
       return template('distable', rows=mydata)
 
 run(host='localhost',port=8457, debug=True)
